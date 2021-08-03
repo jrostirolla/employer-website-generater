cd2c1995c297cfcd8110b1b867b588d708becccf
James Rostirolla <j_rostirolla@hotmail.com>
2021-08-03 11:24:53 +1000

    initial file generation

---
+++ assets/template.js
@@ -0,0 +1,20 @@
+let html = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Our Funky HTML Page</title>
+    <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template">
+</head>
+
+<body>
+    <header>
+    
+    </header>
+</body>
+</html>
+`
+
+
+
+module.exports = Template;
---
+++ index.js
@@ -0,0 +1,8 @@
+//modules
+const Template = require('./template')
+
+//node packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
